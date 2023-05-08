742b5fba0d5971fb15da8ada9d1881b6d8d36674
arunhunt <41851684+arunhunt@users.noreply.github.com>
2023-05-08 12:05:25 +0530

    Update before_install.sh

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo cp -r /var/www/html/new/nginx.conf /etc/nginx/sites-available/default
+sudo cp -r /var/www/html/nginx.conf /etc/nginx/sites-available/default
 sudo systemctl restart nginx.service
